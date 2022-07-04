5bcb0eae1d3842302cc47717d90d97373a728c8e
Yixuan Yang <yyang55@ncsu.edu>
2022-07-04 14:19:42 -0400

    update

--- code/Read_data.R
+++ code/Read_data.R
@@ -1,34 +1,34 @@
 # Remove old Rdata files
-file.remove("./Rdata/ForwardFish.Rdata")
-file.remove("./Rdata/CenterFish.Rdata")
+# file.remove("./Rdata/ForwardFish.Rdata")
+# file.remove("./Rdata/CenterFish.Rdata")
 
 # read in data
 # forward differentiation
-rm(list = ls())
-source('./code/Functions.R')
-## single analysis
-### IGC1
-path = './input_files/ForwardDiff/SingleAnalysis/'
-IGC1_Full = IGC_summary(str_c(path, 'IGC1_Full'))
-IGC1_Full_Force = IGC_summary(str_c(path, 'IGC1_Full_Force'))
-IGC1_Full_Force_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_Force_igcOmega08'))
-IGC1_Full_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_igcOmega08'))
-IGC1_Full_reparameter = IGC_summary(str_c(path, 'IGC1_Full_reparameter1'))
-IGC1_Swap_Clock = IGC_summary(str_c(path, 'IGC1_Swap_Clock'))
-IGC1_Unswap_Clock = IGC_summary(str_c(path, 'IGC1_Unswap_Clock'))
-IGC1_Swap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Swap_Clock_igcOmega08'))
-IGC1_Unswap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_igcOmega08'))
-IGC1_Swap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Swap_Clock_reparameter1'))
-IGC1_Unswap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_reparameter1'))
-
-## joint analysis
-path = './input_files/ForwardDiff/JointAnalysis/'
-Joint_2omega = joint_summary(str_c(path, 'Joint_2omega_newModel'))
-Joint_igcOmega = joint_summary(str_c(path, 'Joint_igcOmega_newModel'))
-Joint_omega = joint_summary(str_c(path, 'Joint_omega'))
-Joint_omega_new = joint_summary(str_c(path, 'Joint_omega_newModel'))
-
-save.image(file = "./Rdata/ForwardFish.Rdata")
+# rm(list = ls())
+# source('./code/Functions.R')
+# ## single analysis
+# ### IGC1
+# path = './input_files/ForwardDiff/SingleAnalysis/'
+# IGC1_Full = IGC_summary(str_c(path, 'IGC1_Full'))
+# IGC1_Full_Force = IGC_summary(str_c(path, 'IGC1_Full_Force'))
+# IGC1_Full_Force_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_Force_igcOmega08'))
+# IGC1_Full_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_igcOmega08'))
+# IGC1_Full_reparameter = IGC_summary(str_c(path, 'IGC1_Full_reparameter1'))
+# IGC1_Swap_Clock = IGC_summary(str_c(path, 'IGC1_Swap_Clock'))
+# IGC1_Unswap_Clock = IGC_summary(str_c(path, 'IGC1_Unswap_Clock'))
+# IGC1_Swap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Swap_Clock_igcOmega08'))
+# IGC1_Unswap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_igcOmega08'))
+# IGC1_Swap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Swap_Clock_reparameter1'))
+# IGC1_Unswap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_reparameter1'))
+# 
+# ## joint analysis
+# path = './input_files/ForwardDiff/JointAnalysis/'
+# Joint_2omega = joint_summary(str_c(path, 'Joint_2omega_newModel'))
+# Joint_igcOmega = joint_summary(str_c(path, 'Joint_igcOmega_newModel'))
+# Joint_omega = joint_summary(str_c(path, 'Joint_omega'))
+# Joint_omega_new = joint_summary(str_c(path, 'Joint_omega_newModel'))
+# 
+# save.image(file = "./Rdata/ForwardFish.Rdata")
 
 
 # center differentiation
@@ -37,24 +37,30 @@ source('./code/Functions.R')
 ## single analysis
 ### IGC1
 path = './input_files/CenterDiff/SingleAnalysis/'
-IGC1_Full = IGC_summary(str_c(path, 'IGC1_Full'))
-IGC1_Full_Force = IGC_summary(str_c(path, 'IGC1_Full_Force'))
-IGC1_Full_Force_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_Force_igcOmega08'))
+IGC1_Full = IGC_summary(str_c(path, 'IGC1_Full')) # original model
+IGC1_Full_ForceTau = IGC_summary(str_c(path, 'IGC1_Full_Force'))
+IGC1_Full_ForceTau_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_Force_igcOmega08'))
 IGC1_Full_igcOmega = IGC_summary(str_c(path, 'IGC1_Full_igcOmega08'))
-IGC1_Full_reparameter = IGC_summary(str_c(path, 'IGC1_Full_reparameter1'))
+IGC1_Full_tauOmega = IGC_summary(str_c(path, 'IGC1_Full_reparameter1'))
 IGC1_Swap_Clock = IGC_summary(str_c(path, 'IGC1_Swap_Clock'))
 IGC1_Unswap_Clock = IGC_summary(str_c(path, 'IGC1_Unswap_Clock'))
 IGC1_Swap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Swap_Clock_igcOmega08'))
 IGC1_Unswap_Clock_igcOmega = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_igcOmega08'))
-IGC1_Swap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Swap_Clock_reparameter1'))
-IGC1_Unswap_Clock_reparameter = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_reparameter1'))
+IGC1_Swap_Clock_tauOmega = IGC_summary(str_c(path, 'IGC1_Swap_Clock_reparameter1'))
+IGC1_Unswap_Clock_tauOmega = IGC_summary(str_c(path, 'IGC1_Unswap_Clock_reparameter1'))
 IGC1_Full_igcOmega0 = IGC_summary(str_c(path, 'IGC1_Full_igcOmega0'))
 IGC1_Full_igcOmega0_tau0 = IGC_summary(str_c(path, 'IGC1_Full_igcOmega0_tau0'))
+# add Homo Omega Model
+path = './input_files/HomoModel/SingleAnalysis_Homo/'
+IGC1_Full_Homo = IGC_summary(str_c(path, 'IGC1_Full_Homo'))
+IGC1_Full_forceTau_homoOmega = IGC_summary(str_c(path, 'IGC1_Full_forceTau_homoOmega'))
+IGC1_Swap_Clock_homoOmega = IGC_summary(str_c(path, 'IGC1_swap_clock_homoOmega'))
+IGC1_Unswap_Clock_homoOmega = IGC_summary(str_c(path, 'IGC1_unswap_clock_homoOmega'))
 
 ## joint analysis
 path = './input_files/CenterDiff/JointAnalysis/'
-Joint_2omega = joint_summary(str_c(path, 'Joint_2omega_newModel'))
-Joint_igcOmega = joint_summary(str_c(path, 'Joint_igcOmega_newModel'))
+Joint_2omega_igcOmega = joint_summary(str_c(path, 'Joint_2omega_newModel'))
+Joint_igcOmega_igcOmega = joint_summary(str_c(path, 'Joint_igcOmega_newModel'))
 Joint_omega = joint_summary(str_c(path, 'Joint_omega'))
 Joint_omega_igcOmega = joint_summary(str_c(path, 'Joint_omega_newModel'))
 
@@ -62,5 +68,18 @@ Joint_tau = joint_summary(str_c(path, 'Joint_tau'))
 Joint_tau_igcOmega = joint_summary(str_c(path, 'Joint_tau_igcOmega08'))
 Joint_tau_and_igcOmega = joint_summary(str_c(path, 'Joint_tau+igcOmega08'))
 
-save.image(file = "./Rdata/CenterFish.Rdata")
+save.image(file = "./Rdata/Fish.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ input_files/HomoModel/SingleAnalysis_Homo/IGC1_swap_clock_tauOmega/2214/Run_IS_IGC_Old.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+from IGCexpansion.CodonGeneconv import ReCodonGeneconv
+import os
+
+if __name__ == '__main__':
+
+    name = "Pillar2214"
+
+    paralog = ['01', '02']
+    alignment_file = './' + name + '.fasta'
+    newicktree = './' + name + '.newick'
+
+    Force = None
+    clock = True
+    model = 'MG94'
+    IGC_Omega = None
+    Tau_Omega = 0.9
+    Homo_Omega = None
+
+    save_folder = './save/'
+    save_name = model + name + '.txt'
+    summary_folder = './summary/'
+    summary_name = model + name + "_summary.txt"
+
+    geneconv = ReCodonGeneconv(newicktree, alignment_file, paralog, Model=model,
+                                    IGC_Omega=IGC_Omega,Tau_Omega=Tau_Omega, Homo_Omega = Homo_Omega,
+                                    Force=Force, clock=clock,
+                                    save_path=save_folder, save_name=save_name)
+    geneconv.get_mle()
+    geneconv.get_ExpectedNumGeneconv()
+    geneconv.get_individual_summary(summary_path=summary_folder)
+
