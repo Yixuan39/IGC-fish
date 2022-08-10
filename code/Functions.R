66c1edf7835e99ea04923d2c87efb7e202ebfd16
Yixuan Yang <yyang55@ncsu.edu>
2022-08-09 21:04:21 -0400

    use str_sort()

--- code/Functions.R
+++ code/Functions.R
@@ -149,7 +149,7 @@ joint_summary = function(file){
 
 
 IGC_summary = function(directory){
-  cases = as.character(sort(as.numeric(list.files(directory))))
+  cases = str_sort(list.files(directory), numeric = TRUE)
   output = c()
   for(case in cases){
     folder = str_c(directory, "/", case, "/summary")
@@ -167,15 +167,55 @@ IGC_summary = function(directory){
   }
   output = matrix(output, ncol = length(cases))
   rownames(output) = name
-  colnames(output) = str_c("Pillar", cases)
+  colnames(output) = cases
   output = t(output) %>% as.data.frame()
   # exclude 6 data
-  `%ni%` <- Negate(`%in%`)
-  output = output %>% filter(row.names(output) %ni%
-                               c("Pillar852",	"Pillar4287", "Pillar561",
-                                 "Pillar2321", "Pillar3278", "Pillar3994"))
+  # `%ni%` <- Negate(`%in%`)
+  # output = output %>% filter(row.names(output) %ni%
+  #                              c("Pillar852",	"Pillar4287", "Pillar561",
+  #                                "Pillar2321", "Pillar3278", "Pillar3994"))
   return(output)
 }
 
-
+# Calculate proportion
+igc.proportion <- function(dataset){
+  out = c()
+  namelist <- colnames(dataset)
+  one2two <- c()
+  two2one <- c()
+  mut <- c()
+  for(i in namelist){
+    if(str_detect(i,"1->2")){
+      if (str_detect(i,"N0")==FALSE){
+        one2two <- append(one2two,i)
+      }
+    }
+    
+    if(str_detect(i,"2->1")){
+      if (str_detect(i,"N0")==FALSE){
+        two2one <- append(two2one,i)
+      }
+    }
+    
+    if(str_detect(i,"mut")){
+      if (str_detect(i,"N0")==FALSE){
+        mut <- append(mut,i)
+      }
+    }
+  }
+  for(j in 1:dim(dataset)[1]){
+    one2twoSum <- sum(dataset[j, one2two])
+    two2oneSum <- sum(dataset[j, two2one])
+    mutSum <- sum(dataset[j, mut])
+    num <- sum(one2twoSum, two2oneSum)
+    den <- sum(num, mutSum)
+    p = num/den
+    out = rbind(out,c(rownames(dataset)[j], p))
+  }
+  out = data.frame(out)
+  names(out) = c("names","proportion")
+  out[,2] = as.numeric(out[,2])
+  out = data.frame(out, row.names = 1)
+  return(out)
+}
 
