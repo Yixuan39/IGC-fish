6e9f556543d2699fd383eb1e137703e702067aca
Yixuan Yang <yyang55@ncsu.edu>
2022-08-16 18:08:36 -0400

    add results from IGCexpansion

---
+++ input_files/CenterDiff/SingleAnalysis/IGC1_Full_forceTau_update/3994/Run_IS_IGC_Old.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+from IGCexpansion.CodonGeneconv import ReCodonGeneconv
+import os
+
+if __name__ == '__main__':
+
+    name = "Pillar3994"
+
+    paralog = ['01', '02']
+    alignment_file = './' + name + '.fasta'
+    newicktree = './' + name + '.newick'
+
+    Force = {5:0}
+    clock = None
+    model = 'MG94'
+    IGC_Omega = None
+    Tau_Omega = None
+    Homo_Omega = None
+
+    save_folder = './save/'
+    save_name = model + name + '.txt'
+    summary_name = model + name + "_summary.txt"
+
+    summary_folder = './summary/'
+
+    geneconv = ReCodonGeneconv(newicktree, alignment_file, paralog, Model=model,
+                               tau=1e-6,
+                               IGC_Omega=IGC_Omega,Tau_Omega=Tau_Omega, Homo_Omega=Homo_Omega,
+                               Force=Force, clock=clock,
+                               save_path=save_folder, save_name=save_name)
+    geneconv.get_mle()
+    geneconv.get_ExpectedNumGeneconv()
+    geneconv.get_individual_summary(summary_path=summary_folder)
+
